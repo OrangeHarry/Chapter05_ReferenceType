7df0fd841d8eab42a52c164d9a3efc88d9fa13d0
bitcamp <bitcamp@DESKTOP-6KHHUGQ>
2021-07-29 19:23:18 +0900

    tt

---
+++ src/ch05_2_array/AdvancedForExample.java
@@ -0,0 +1,19 @@
+package ch05_2_array;
+
+public class AdvancedForExample {
+	public static void main(String[] args) {
+		int[] scores = { 95, 71, 84, 93, 87 };
+		int sum = 0;
+
+		for (int score : scores) {
+			sum += score;
+		}
+//		for (int i = 0; i < scores.length; i++) {
+//			sum += scores[i];
+//		}
+		System.out.println("Á¡¼ö ÃÑÇÕ = " + sum);
+
+		double avg = (double) sum / 5;
+		System.out.println("Æò±Õ = " + avg);
+	}
+}
